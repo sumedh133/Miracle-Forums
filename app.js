c23985da022c6939e6420a43b7c24c8163c23623
sumedh133 <sumedh133bhamre@gmail.com>
2023-06-04 16:47:53 +0530

    session based authentication

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const bcrypt=require("bcrypt");
-const salt_rounds=10;
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const app = express();
 
@@ -14,11 +15,25 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/",function(req,res){
-  res.render("loginRegistration");
-});
+app.use(session({
+  secret:process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false,
+}));
 
-mongoose.connect("mongodb://0.0.0.0:27017/userDB", {useNewUrlParser: true});
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://0.0.0.0/myapp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -31,51 +46,102 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User= mongoose.model("User",userSchema);
 
-app.post('/register', async (req, res) => {
-  try {
-    const password=await bcrypt.hash(req.body.password, salt_rounds);
-    const { username, email } = req.body;
-    const user = new User({ username, email, password });
-    await user.save();
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/",function(req,res){
+  res.render("loginRegistration");
+});
+
+app.get("/feed",function(req,res){
+  if(req.isAuthenticated()){
     res.render("feed");
-    } 
-    catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({ error: 'Internal server error' });
+  }
+  else{
+    res.redirect("/");
   }
 });
 
-app.post('/login', async (req, res) => {
-  try {
-    const username_email = req.body.username_email;
-    
-    const user = await User.findOne({
-      $or: [{ username: username_email }, { email: username_email }],
+app.get('/logout', function(req, res, next) {
+  //clicking on logout button will no longer accept the cookie as valid
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+app.post('/register', (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({ username, email });
+
+  User.register(newUser, password, (err, user) => {
+    if (err) {
+      if (err.message.includes('username')) {
+        res.status(400).json({ message: 'Username is already taken' });
+      } else if (err.message.includes('email')) {
+        res.status(400).json({ message: 'Email is already taken' });
+      }
+      console.error('Failed to register user', err);
+      res.status(500).json({ message: 'Failed to register user' });
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/feed");
+      });
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { usernameOrEmail, password, rememberMe } = req.body;
+
+  // searching user by username or email
+  User.findOne({ $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] })
+    .then((user) => {
+      if (!user) {
+        // Invalid username or email
+        res.status(401).json({ message: 'Invalid username or email' });
+      }
+
+      // authenticating the user using the provided password
+      user.authenticate(password)
+        .then((authenticated) => {
+          if (!authenticated) {
+            // invalid password
+            res.status(401).json({ message: 'Invalid password' });
+          }
+          // logging in the user
+          req.login(user, (err) => {
+            if (err) {
+              // error during login
+              console.error('Error during login', err);
+              res.status(500).json({ message: 'Error during login' });
+            }else{
+              if(rememberMe)
+              req.session.cookie.expires = new Date(Date.now() + 3600000*24*30);
+            // if successful 
+              res.redirect("/feed");
+            }
+          });
+        });
+    })
+    .catch((err) => {
+      // Error during authentication
+      console.error('Error during authentication', err);
+      res.status(500).json({ message: 'Error during authentication' });
     });
-    const password_check=await bcrypt.compare(req.body.password, user.password);
-
-    if (!user) {
-      return res.status(401).json({ error: 'User doesn\'t exist' });
-    }
-    if (!password_check){
-      return res.status(402).json({ error: 'Invalid password' });     
-    }
-    res.render("feed");
-    }
-    catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
 });
+
 
 app.listen(5000, function() {
+  //checking working port
     console.log("Server started on port 5000.");
 });
--- public/javascript/loginRegistration.js
+++ public/javascript/loginRegistration.js
@@ -16,3 +16,13 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+// Change the type of input to password or text
+function Toggle() {
+    var temp = document.getElementById("typepass");
+    if (temp.type === "password") {
+        temp.type = "text";
+    }
+    else {
+        temp.type = "password";
+    }
+}
