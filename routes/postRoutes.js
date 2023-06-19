c0b02973b9e77d7a79ce26cde5e8162a9ecb765e
sumedh133 <sumedh133bhamre@gmail.com>
2023-06-19 20:55:52 +0530

    Tag suggestions implemented

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -43,6 +43,20 @@ router.post("/create", async (req, res) => {
   }
 });
 
+router.get('/tag/suggestions', async (req, res) => {
+  try {
+    const searchText = req.query.searchText; // Get the entered text from the query parameter
+
+    // Perform a search in the tags collection based on the entered text
+    const matchedTags = await Tag.find({ name: { $regex: searchText, $options: 'i' } });
+
+    res.json(matchedTags); // Return the matched tags as the response
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error retrieving tag suggestions' });
+  }
+});
+
 
 
 module.exports = router;
